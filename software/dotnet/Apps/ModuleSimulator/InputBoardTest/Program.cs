57b934f9ede134d5b0978a445f7eab88af59a79f
Chris Tacke <ctacke@gmail.com>
2026-05-28 18:41:48 -0500

    feat: input board test

---
+++ software/dotnet/Apps/ModuleSimulator/InputBoardTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using Meadow;
+
+internal partial class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        await MeadowOS.Start<MeadowApp>();
+    }
+}
