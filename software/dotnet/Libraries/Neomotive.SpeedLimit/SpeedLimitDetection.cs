d6c1d506fcd69ef0d4ff95a94aaa7d9e736e98cb
Chris Tacke <ctacke@gmail.com>
2026-06-20 10:57:36 -0500

    speed limit library files

---
+++ software/dotnet/Libraries/Neomotive.SpeedLimit/SpeedLimitDetection.cs
@@ -0,0 +1,11 @@
+﻿using OpenCvSharp;
+
+namespace NeoMotive.Services;
+
+public class SpeedLimitDetection
+{
+    public int SpeedLimit { get; set; }
+    public float Confidence { get; set; }
+    public Rect2d BoundingBox { get; set; }
+    public string Label { get; set; } = string.Empty;
+}
---
+++ software/dotnet/Libraries/Neomotive.SpeedLimit/SpeedLimitDetectionGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace NeoMotive.Services;
+
+public class SpeedLimitDetectionGroup
+{
+    public int SpeedLimit { get; set; }
+    public float HighestConfidence { get; set; }
+    public int DetectionCount { get; set; }
+    public DateTime FirstDetected { get; set; }
+    public DateTime LastDetected { get; set; }
+}
